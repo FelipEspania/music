6697f385e414d592fd394fd63bc80480cd19b7e3
suiciknot <felipe.espana1988@gmail.com>
2017-04-10 12:33:56 -0300

    start angular2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'Music!';
   public user: User;
-  public identity = true;
+  public identity;
   public token;
+  public errorMessage;
 
-  constructor(){
+  constructor(
+    private _userService:UserService
+  ){
     this.user = new User('','','','','','ROLE_USER','');
   }
 
+  ngOnInit(){
+    
+  }
+
+  public onSubmit(){
+    console.log(this.user);
+    //Conseguir los datos del usuario identificado
+    this._userService.signup(this.user).subscribe(
+      response => {
+        let identity = response.user;
+        this.identity = identity;
+
+        if (!this.identity._id) {
+          alert("El usuario no esta correctamente logeado");
+        }else{
+          // Crear elemento en el localStorage para tener al usuario sesion
+
+          // Conseguir el token para enviarselo a cada peticion http
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+
+              if (this.token.length <= 0) {
+                alert("El token no se ha generado correctamente");
+              }else{
+                // Crear elemento en el localStorage para tener token disponible
+
+                // Conseguir el token para enviarselo a cada peticion http
+                console.log(token);
+                console.log(identity);              
+              }
+            },
+            error => {
+              var errorMessage = <any>error;
+              if (errorMessage != null) {
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.message;
+                console.log(error);
+              }
+            }
+          );
+        }
+      },
+      error => {
+        var errorMessage = <any>error;
+        
+        if (errorMessage != null) {
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
+          console.log(error);
+        }
+      }
+    );
+  }
 }
